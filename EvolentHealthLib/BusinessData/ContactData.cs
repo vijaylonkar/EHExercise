c8d887f4d4524cce7d84a43239fff4392627b7ca
vijaylonkar <64409838+vijaylonkar@users.noreply.github.com>
2020-04-29 21:02:02 +0530

    Project EvolentHealthLib

---
+++ EvolentHealthLib/BusinessData/ContactData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EvolentHealthLib.BusinessData
+{
+	[DataContract]
+	public class ContactData
+	{
+		[DataMember(Name ="id")]
+		public Int64 ContactID { get; set; }
+		[DataMember(Name = "fn")]
+		public string FirstName { get; set; }
+		[DataMember(Name = "ln")]
+		public string LastName { get; set; }
+		[DataMember(Name = "mid")]
+		public string EmailAddress { get; set; }
+		[DataMember(Name = "pn")]
+		public string PhoneNumber { get; set; }
+		[DataMember(Name = "st")]
+		public byte Status { get; set; }
+
+		public ContactData()
+		{
+
+		}
+		public ContactData(Int64 id, string firstName, string lastName, string emailAddress, string phoneNumber, byte status)
+		{
+			this.ContactID = id;
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.EmailAddress = emailAddress;
+			this.PhoneNumber = phoneNumber;
+			this.Status = status;
+		}
+	}
+}
---
+++ EvolentHealthLib/Messages/BaseResponse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EvolentHealthLib.Messages
+{
+	[DataContract]
+	public class BaseResponse
+	{
+		#region Response Values
+
+		public const int RESPONSE_SUCCESS = 0;
+
+		public const int RESPONSE_INITIALIZE = -1;
+
+		public const int RESPONSE_NULLOBJECTS = -11;
+		public const int RESPONSE_INVALIDREQUEST = -12;
+		public const int RESPONSE_RUNTIMEEXCEPTION = -13;
+
+		#endregion
+
+		public BaseResponse()
+		{
+			this.ReturnValueInt = RESPONSE_INITIALIZE;
+			this.ReturnValueString = String.Empty;
+			this.ReturnValueCustomString = String.Empty;
+		}
+		public void Success()
+		{
+			this.ReturnValueInt = RESPONSE_SUCCESS;
+			this.ReturnValueString = String.Empty;
+			this.ReturnValueCustomString = String.Empty;
+		}
+		public void Success(object customObject)
+		{
+			this.ReturnValueInt = RESPONSE_SUCCESS;
+			this.ReturnValueString = String.Empty;
+			this.ReturnValueCustomString = String.Empty;
+			this.ReturnValueCustomObject = customObject;
+		}
+		public void Success(string customString)
+		{
+			this.ReturnValueInt = RESPONSE_SUCCESS;
+			this.ReturnValueString = String.Empty;
+			this.ReturnValueCustomString = customString;
+		}
+		public void RunTimeException(Exception e, string customString)
+		{
+			this.ReturnValueInt = RESPONSE_RUNTIMEEXCEPTION;
+			this.ReturnValueString = e.Message;
+			this.ReturnValueCustomString = customString;
+		}
+
+		[DataMember(Name = "RVI")]
+		public int ReturnValueInt { get; set; } // return value int
+
+		[DataMember(Name = "RVS")]
+		public string ReturnValueString { get; set; } // return value string
+
+		[DataMember(Name = "RCS")]
+		public string ReturnValueCustomString { get; set; } // return message: custom message
+
+		[DataMember(Name = "RCO")]
+		public object ReturnValueCustomObject { get; set; } // return Custom Object
+	}
+}
