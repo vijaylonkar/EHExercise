cc3ae3805c4fd1c6611941bd2b74c0e02edb79da
vijaylonkar <64409838+vijaylonkar@users.noreply.github.com>
2020-04-29 21:04:19 +0530

    Project EvolentHealthAPI.Tests

---
+++ EvolentHealthAPI.Tests/Controllers/ContactsControllerTest.cs
@@ -0,0 +1,317 @@
+﻿using System.Threading.Tasks;
+using EvolentHealthAPI.Controllers;
+using EvolentHealthAPI.Tests.Mock;
+using EvolentHealthAPICore.Interfaces;
+using EvolentHealthAPILib.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository.Interfaces;
+using Repository.Services;
+using EvolentHealthLib.Messages;
+
+namespace EvolentHealthAPI.Tests.Controllers
+{
+	[TestClass]
+	public class ContactsControllerTest
+	{
+		#region GetContacts Tests
+
+		[TestMethod]
+		public async Task GetContacts_Success()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			GetContactsRequest request = new GetContactsRequest();
+
+			// Act
+			GetContactsResponse response = await (controller.GetContacts(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(4, response.ContactDataList.Count);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_SUCCESS);
+			Assert.AreEqual(response.ReturnValueCustomString, "Contact Get Successful.");
+		}
+
+		#endregion
+
+		#region AddContact Tests
+
+		[TestMethod]
+		public async Task AddContact_Success()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			AddContactRequest request = new AddContactRequest();
+			request.FirstName = "Vijay6";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l6@a.in";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			AddContactResponse response = await (controller.AddContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.NewContactID, 6);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_SUCCESS);
+			Assert.AreEqual(response.ReturnValueCustomString, "Contact added Successfully.");
+		}
+
+		[TestMethod]
+		public async Task AddContact_NullInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+
+			// Act
+			AddContactResponse response = await (controller.AddContact(null));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_NULLOBJECTS);
+			Assert.AreEqual(response.ReturnValueCustomString, "NULL Request object.");
+		}
+		
+		[TestMethod]
+		public async Task AddContact_InValidInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			AddContactRequest request = new AddContactRequest();
+			request.FirstName = "";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l4a.in";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			AddContactResponse response = await (controller.AddContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_INVALIDREQUEST);
+			Assert.AreEqual(response.ReturnValueCustomString, "FirstName is empty.InValid Email Address.");
+		}
+
+		[TestMethod]
+		public async Task AddContact_EmailDuplicate()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			AddContactRequest request = new AddContactRequest();
+			request.FirstName = "Vijay6";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l4@a.in";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			AddContactResponse response = await (controller.AddContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_RUNTIMEEXCEPTION);
+			Assert.AreEqual(response.ReturnValueString, "Email Address already Exists");
+			Assert.AreEqual(response.ReturnValueCustomString, "Exception in Add");
+		}
+
+		#endregion
+
+		#region EditContact Tests
+
+		[TestMethod]
+		public async Task EditContact_Success()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			EditContactRequest request = new EditContactRequest();
+			request.ContactID = 2;
+			request.FirstName = "Vijay6";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l6@gmail.com";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			EditContactResponse response = await (controller.EditContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_SUCCESS);
+			Assert.AreEqual(response.ReturnValueCustomString, "Contact Edited Successfully.");
+		}
+
+		[TestMethod]
+		public async Task EditContact_NullInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+
+			// Act
+			EditContactResponse response = await (controller.EditContact(null));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_NULLOBJECTS);
+			Assert.AreEqual(response.ReturnValueCustomString, "NULL Request object.");
+		}
+
+		[TestMethod]
+		public async Task EditContact_InValidInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			EditContactRequest request = new EditContactRequest();
+			request.ContactID = -1;
+			request.FirstName = "";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l4a.in";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			EditContactResponse response = await (controller.EditContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_INVALIDREQUEST);
+			Assert.AreEqual(response.ReturnValueCustomString, "Invalid Contact ID.FirstName is empty.InValid Email Address.");
+		}
+
+		[TestMethod]
+		public async Task EditContact_RowNotFound()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			EditContactRequest request = new EditContactRequest();
+			request.ContactID = 1000;
+			request.FirstName = "Vijay6";
+			request.LastName = "Lonkar6";
+			request.EmailAddress = "v.l4@a.in";
+			request.PhoneNumber = "+91 611.111.111";
+
+			// Act
+			EditContactResponse response = await (controller.EditContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_RUNTIMEEXCEPTION);
+			Assert.AreEqual(response.ReturnValueString, "Row not found");
+			Assert.AreEqual(response.ReturnValueCustomString, "Exception in Edit");
+		}
+
+		#endregion
+
+		#region DeleteContact Test
+
+		[TestMethod]
+		public async Task DeleteContact_Success()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			DeleteContactRequest request = new DeleteContactRequest();
+			request.ContactID = 3;
+
+			// Act
+			DeleteContactResponse response = await (controller.DeleteContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_SUCCESS);
+			Assert.AreEqual(response.ReturnValueCustomString, "Contact Deleted Successfully.");
+		}
+
+		[TestMethod]
+		public async Task DeleteContact_NullInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+
+			// Act
+			DeleteContactResponse response = await (controller.DeleteContact(null));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_NULLOBJECTS);
+			Assert.AreEqual(response.ReturnValueCustomString, "NULL Request object.");
+		}
+
+		[TestMethod]
+		public async Task DeleteContact_InValidInput()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			DeleteContactRequest request = new DeleteContactRequest();
+			request.ContactID = -1;
+
+			// Act
+			DeleteContactResponse response = await (controller.DeleteContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_INVALIDREQUEST);
+			Assert.AreEqual(response.ReturnValueCustomString, "InValid Contact ID.");
+		}
+
+		[TestMethod]
+		public async Task DeleteContact_RowNotFound()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			DeleteContactRequest request = new DeleteContactRequest();
+			request.ContactID = 1000;
+
+			// Act
+			DeleteContactResponse response = await (controller.DeleteContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_RUNTIMEEXCEPTION);
+			Assert.AreEqual(response.ReturnValueString, "Row not found");
+			Assert.AreEqual(response.ReturnValueCustomString, "Exception in Delete");
+		}
+
+		[TestMethod]
+		public async Task DeleteContact_ActiveRowNotFound()
+		{
+			// Arrange
+			ICore core = new MockCore();
+			ILogger logger = new Logger();
+			ContactsController controller = new ContactsController(core, logger);
+			DeleteContactRequest request = new DeleteContactRequest();
+			request.ContactID = 2;
+
+			// Act
+			DeleteContactResponse response = await (controller.DeleteContact(request));
+
+			// Assert
+			Assert.IsNotNull(response);
+			Assert.AreEqual(response.ReturnValueInt, BaseResponse.RESPONSE_RUNTIMEEXCEPTION);
+			Assert.AreEqual(response.ReturnValueString, "Row not found");
+			Assert.AreEqual(response.ReturnValueCustomString, "Exception in Delete");
+		}
+		#endregion
+	}
+}
---
+++ EvolentHealthAPI.Tests/Mock/MockCore.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using EvolentHealthAPICore.BusinessLogic.Interfaces;
+using EvolentHealthAPICore.Factories;
+using EvolentHealthAPICore.Interfaces;
+using EvolentHealthAPILib.Messages;
+using EvolentHealthLib.BusinessData;
+using Repository.Interfaces;
+
+namespace EvolentHealthAPI.Tests.Mock
+{
+	public class MockCore : ICore
+	{
+
+		private List<ContactData> contactDataList;
+		public MockCore()
+		{
+			this.contactDataList = new List<ContactData>();
+
+			contactDataList.Add(new ContactData(1, "vijay1", "lonkar1", "v.l1@a.in", "+91 111.111.1111", 1));
+			contactDataList.Add(new ContactData(2, "vijay2", "lonkar2", "v.l2@a.in", "+91 211.111.1111", 0));
+			contactDataList.Add(new ContactData(3, "vijay3", "", "v.l3@a.in", "+91 311.111.1111", 1));
+			contactDataList.Add(new ContactData(4, "vijay4", "lonkar4", "v.l4@a.in", "+91 411.111.1111", 1));
+			contactDataList.Add(new ContactData(5, "vijay5", "lonkar5", "v.l5@a.in", "+91 511.111.1111", 1));
+		}
+
+		public Task<GetContactsResponse> GetContacts(GetContactsRequest getContactsRequest)
+		{
+			return Task.Run(() =>
+			{
+				GetContactsResponse response = new GetContactsResponse();
+
+				response.ContactDataList = contactDataList.FindAll(c => c.Status == 1);
+				response.HTTPReturnCode = (Int16)HttpStatusCode.OK;
+				response.APIVersion = "1.0.0.0";
+				response.ReturnValueInt = 0;
+				response.ReturnValueString = "";
+				response.ReturnValueCustomString = "Contact Get Successfully.";
+				response.ReturnValueCustomObject = null;
+
+				return response;
+			});
+		}
+
+		public Task<AddContactResponse> AddContact(AddContactRequest addContactRequest)
+		{
+			return Task.Run(() =>
+			{
+				AddContactResponse response = new AddContactResponse();
+
+				if (this.contactDataList.Exists(c => c.EmailAddress == addContactRequest.EmailAddress))
+				{
+					response.NewContactID = 0;
+					response.ReturnValueInt = -13;
+					response.ReturnValueString = "Email Address already Exists";
+					response.ReturnValueCustomString = "Exception in Add";
+				}
+				else
+				{
+					this.contactDataList.Add(new ContactData(6,
+															addContactRequest.FirstName,
+															addContactRequest.LastName,
+															addContactRequest.EmailAddress,
+															addContactRequest.PhoneNumber,
+															1));
+					response.NewContactID = 6;
+					response.ReturnValueInt = 0;
+					response.ReturnValueString = "";
+					response.ReturnValueCustomString = "Contact added Successfully.";
+				}
+
+				response.HTTPReturnCode = (Int16)HttpStatusCode.OK;
+				response.APIVersion = "1.0.0.0";
+				response.ReturnValueCustomObject = null;
+
+				return response;
+			});
+		}
+
+		public Task<EditContactResponse> EditContact(EditContactRequest editContactRequest)
+		{
+			return Task.Run(() =>
+			{
+				EditContactResponse response = new EditContactResponse();
+
+				ContactData editContactData = this.contactDataList.Find(c => c.ContactID == editContactRequest.ContactID);
+
+				if (editContactData != null)
+				{
+					editContactData.FirstName = editContactRequest.FirstName;
+					editContactData.LastName = editContactRequest.LastName;
+					editContactData.EmailAddress = editContactRequest.EmailAddress;
+					editContactData.PhoneNumber = editContactRequest.PhoneNumber;
+
+					response.ReturnValueInt = 0;
+					response.ReturnValueString = "";
+					response.ReturnValueCustomString = "Contact Edited Successfully.";
+				}
+				else
+				{
+					response.ReturnValueInt = -13;
+					response.ReturnValueString = "Row not found";
+					response.ReturnValueCustomString = "Exception in Edit";
+				}
+
+				response.HTTPReturnCode = (Int16)HttpStatusCode.OK;
+				response.APIVersion = "1.0.0.0";
+				response.ReturnValueCustomObject = null;
+
+				return response;
+			});
+		}
+
+		public Task<DeleteContactResponse> DeleteContact(DeleteContactRequest deleteContactRequest)
+		{
+			return Task.Run(() =>
+			{
+				DeleteContactResponse response = new DeleteContactResponse();
+
+				ContactData editContactData =
+					this.contactDataList.Find(c => c.ContactID == deleteContactRequest.ContactID && c.Status == 1);
+
+				if (editContactData != null)
+				{
+					editContactData.Status = 0;
+
+					response.ReturnValueInt = 0;
+					response.ReturnValueString = "";
+					response.ReturnValueCustomString = "Contact Edited Successfully.";
+				}
+				else
+				{
+					response.ReturnValueInt = -13;
+					response.ReturnValueString = "Row not found";
+					response.ReturnValueCustomString = "Exception in Delete";
+				}
+
+				response.HTTPReturnCode = (Int16)HttpStatusCode.OK;
+				response.APIVersion = "1.0.0.0";
+				response.ReturnValueCustomObject = null;
+
+				return response;
+			});
+		}
+	}
+}
